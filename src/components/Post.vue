8d90756209b7f123ebfbe811b0faa4a052ddd9e0
McOwska <zuza.m.makowska@gmail.com>
2023-11-21 17:16:34 +0200

    post component

---
+++ src/components/Post.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="post">
+    <img class="icon" :src="post.usericon">
+    <h2>{{ post.title }}</h2>
+    <p>{{ post.content }}</p>
+    <img class="image" :src="post.image">
+    <div class="post-footer">
+      <span class="author">{{ post.author }}</span>
+      <span class="date">{{ post.date }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostComponent',
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+.icon{
+    max-width: 50px;
+    border-radius: 100%;
+    border: 3px solid rgba(235, 223, 183, 0.758);
+}
+.image{
+    max-width: 100%;
+    width: 100%;
+    height: auto;
+    display: block;
+}
+</style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,53 +1,48 @@
 <template>
-    <div class="grid">
-      <div class="sidebar"></div>
-      <div class="container">
-        <!-- Iterate over posts and display them -->
-        <div v-for="post in posts" :key="post.content">
-            <div class="post">
-                <img class="icon" :src="post.usericon">
-                <p class="date">{{post.date}}</p>
-                <p>{{ post.content }}</p>
-                <img class="img" :src="post.image">
-            </div>
-        </div>
-  
-        <!-- Display error message if there is an error -->
-        <div v-if="error" class="error">{{ error }}</div>
+  <div class="grid">
+    <div class="sidebar"></div>
+    <div class="container">
+      <div v-for="post in posts" :key="post.id">
+        <Post :post="post" />
       </div>
-      <div class="sidebar"></div>
+      <div v-if="error" class="error">{{ error }}</div>
     </div>
-  </template>
+    <div class="sidebar"></div>
+  </div>
+</template>
   
   
-  <script>
-  export default {
-    name: 'MainPage',
-    data() {
-      return {
-        posts: [],
-        error: null
-      };
-    },
-    mounted() {
-      this.fetchPosts();
-    },
-    methods: {
-      fetchPosts() {
-        fetch('https://api.npoint.io/e49bbe98e965d535a9ce')
-          .then(response => response.json())
-          .then(data => {
-            this.posts = data;
-          })
-          .catch(error => {
-            this.error = 'Error loading posts: ' + error.message;
-          });
-      }
+<script>
+import Post from '../components/Post.vue';
+
+export default {
+  name: 'MainPage',
+  components: {
+    Post
+  },
+  data() {
+    return {
+      posts: [],
+      error: null
+    };
+  },
+  mounted() {
+    this.fetchPosts();
+  },
+  methods: {
+    fetchPosts() {
+      fetch('https://api.npoint.io/e49bbe98e965d535a9ce')
+        .then(response => response.json())
+        .then(data => {
+          this.posts = data;
+        })
+        .catch(error => {
+          this.error = 'Error loading posts: ' + error.message;
+        });
     }
   }
-  </script>
+}
+</script>
   
-  <style scoped>
-    
-  </style>
+<style scoped></style>
   
