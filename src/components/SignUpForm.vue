c5a2f820560c8399ca878d86939d997755db2250
flemarrgoL <91751451+flemarrgoL@users.noreply.github.com>
2023-11-26 20:33:07 +0200

    ugly signupform component created and added to signuppage view

---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,27 @@
+<template>
+    <footer class="footer">
+      <div class="container">
+        <p>&copy; {{ currentYear }} I am signup form </p>
+      </div>
+    </footer>
+  </template>
+
+<script>
+export default {
+    name: "AppSignUpForm",
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+      dummyText: new String('Sign up here, dummy text'),
+    };
+  },
+};
+</script>
+
+<style scoped>
+.form {
+  background-color: #861414;
+  padding: 20px;
+  text-align: center;
+}
+</style>
--- src/views/SignUpPage.vue
+++ src/views/SignUpPage.vue
@@ -1,13 +1,24 @@
 <template>
     <div class="grid">
       <p>SIGN UP</p>
+    <div class="sidebar"></div>
+    <div class="container">
+    <SignUpForm></SignUpForm>
     </div>
+    <div class="sidebar"></div>
+  </div>
   </template>
   
   
   <script>
+  import SignUpForm from '../components/SignUpForm.vue'
+
+  
   export default {
     name: 'SignUpPage',
+    components: { 
+    SignUpForm,
+  }
   }
   </script>
   
